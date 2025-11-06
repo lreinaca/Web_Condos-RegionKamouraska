cd9e3491e566b8c162b1cdd7f57ac25a8c26af56
LorenaReina <lorenareina2016@gmail.com>
2025-11-05 23:18:01 -0500

    chore(repo): move project to repository root

--- CascadeProjects/windsurf-project/components/CTASticky.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export function CTASticky() {
-  return (
-    <div className="fixed bottom-5 right-5 sm:right-8 z-40">
-      <Link href="/contacto" className="btn shadow-lg">Reservar</Link>
-    </div>
-  );
-}
