8dd8b476c72ccc2717c03ae4978d707d89d047be
LorenaReina <lorenareina2016@gmail.com>
2025-11-07 19:59:31 -0500

    chore(analytics): add Google Tag Manager snippet to head (GTM-TT5QD6M8)

--- CascadeProjects/Web_Condos-RegionKamouraska/app/layout.tsx
+++ CascadeProjects/Web_Condos-RegionKamouraska/app/layout.tsx
@@ -15,6 +15,15 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-TT5QD6M8');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body>
         <Navbar />
         {children}
